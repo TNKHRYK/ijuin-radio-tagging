d47d4af679b803c512edf940fee32b73eb11885c
Tanahiro <trinine3x9@gmail.com>
2018-03-29 22:36:42 +0900

    tag編集機能追加

--- setTag.py
+++ setTag.py
@@ -5,18 +5,26 @@ from mutagen.easyid3 import EasyID3
 import time
 
 
-def show_id3_tags(file_path):
-    tags = EasyID3(file_path)
+def showId3Tags(srcFile):
+    tags = EasyID3(srcFile)
     print(tags.pprint())
 
-def rename(srcName):
+def set_id3_tag(srcFile):
+    tags = EasyID3(srcFile)
+    time_str = time.strftime('%Y%m%d%H%M%S')
+    tags['author'] = 'ちとん'+time_str
+    tags.save()
+    print(tags['author'])
+
+def rename(srcFile):
     #testfile = open(targetPath + "JUNK伊集院光・深夜の馬鹿力2018年03月19日.mp3")
     #content = testfile.readlines()
     #content = testfile.read()
     time_str = time.strftime('%Y%m%d%H%M%S')
-    print(time_str)
-    dstName = srcName.replace('26',time_str)
-    os.rename(srcName,dstName)
+    #print(time_str)
+    dstName = srcFile.replace('26','26')
+    os.rename(srcFile,dstName)
+
 
 
 targetDir = 'test/'
@@ -24,7 +32,9 @@ targetFile = 'JUNK伊集院光・深夜の馬鹿力2018年03月26日.mp3'
 
 os.chdir(targetDir)
 if os.path.exists(targetFile):
-    show_id3_tags(targetFile)
+    showId3Tags(targetFile)
     rename(targetFile)
+    set_id3_tag(targetFile)
+
 else:
     print("ファイルが無いよ")
