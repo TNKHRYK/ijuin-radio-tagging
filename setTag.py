1f6389222a2001d20a346f3c3a9137b4b53fffc6
Tanahiro <trinine3x9@gmail.com>
2018-03-31 01:47:00 +0900

    本番用設定タグ追加

--- setTag.py
+++ setTag.py
@@ -11,10 +11,14 @@ def showId3Tags(srcFile):
 
 def set_id3_tag(srcFile):
     tags = EasyID3(srcFile)
-    time_str = time.strftime('%Y%m%d%H%M%S')
-    tags['author'] = 'ちとん'+time_str
+    time_str = time.strftime('%Y%m%d')
+    tags['author'] = '伊集院 光'
+    tags['artist'] = '伊集院 光'
+    tags['album'] = '深夜の馬鹿力 '+ time.strftime('%Y') +'年'
+    tags['genre'] = 'Radio'
+    tags['date'] = time_str
     tags.save()
-    print(tags['author'])
+    print(tags['date'])
 
 def rename(srcFile):
     #testfile = open(targetPath + "JUNK伊集院光・深夜の馬鹿力2018年03月19日.mp3")
@@ -23,18 +27,18 @@ def rename(srcFile):
     time_str = time.strftime('%Y%m%d%H%M%S')
     #print(time_str)
     dstName = srcFile.replace('26','26')
+    dstName = 'ちとんテスト.mp3'
     os.rename(srcFile,dstName)
 
 
 
 targetDir = 'test/'
-targetFile = 'JUNK伊集院光・深夜の馬鹿力2018年03月26日.mp3'
+targetFile = 'ちとんテスト.mp3' #'JUNK伊集院光・深夜の馬鹿力2018年03月26日.mp3'
 
 os.chdir(targetDir)
 if os.path.exists(targetFile):
     showId3Tags(targetFile)
-    rename(targetFile)
     set_id3_tag(targetFile)
-
+    rename(targetFile)
 else:
     print("ファイルが無いよ")
