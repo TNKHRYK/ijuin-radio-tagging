60c679f23294dc2389dba9f776ec5dec42b5e60a
Tanahiro <trinine3x9@gmail.com>
2019-09-06 11:15:43 +0900

    元mp3が規定ファイル名でないときの処理を追加

--- setTag.py
+++ setTag.py
@@ -27,8 +27,10 @@ def set_id3_tag(srcFile):
 def rename(srcFile):
     #time_str = time.strftime('%Y%m%d%H%M%S')
     #print(time_str)
-    dstName = srcFile.replace('伊集院光 深夜の馬鹿力 ','JUNK伊集院光・深夜の馬鹿力')
-    dstName = dstName
+    if '伊集院光 深夜の馬鹿力' in srcFile
+        dstName = srcFile.replace('伊集院光 深夜の馬鹿力 ','JUNK伊集院光・深夜の馬鹿力')
+    else
+        dstName = 'JUNK伊集院光・深夜の馬鹿力xxxx年xx月xx日'
     os.rename(srcFile,dstName)
 
 argvs = sys.argv
